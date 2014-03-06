428a06e77e38f65cbeb22e55ad5e85c043fad4cd
Andrew <andrewhuth2@live.com>
2014-03-06 15:11:17 -0500

    Converted AssetUrlHelper back

--- lib/emcee.rb
+++ lib/emcee.rb
@@ -9,14 +9,6 @@ require "emcee/railtie"
 
 module ActionView
   module Helpers
-    module AssetUrlHelper
-      include Emcee::Helpers::ActionView::AssetUrlHelper
-
-      # Modify ActionView to recognize html files and the '/elements' path.
-      ASSET_EXTENSIONS.merge!({ html: '.html' })
-      ASSET_PUBLIC_DIRECTORIES.merge!({ html: '/elements' })
-    end
-
     module AssetTagHelper
       include Emcee::Helpers::ActionView::AssetTagHelper
     end
