b0e686c69ce6d92c88ed223e5be9c4840ef805ae
Andrew <andrewhuth2@live.com>
2014-03-05 21:31:24 -0500

    Add dummy route and controller action to render string of application.html

--- test/dummy/app/controllers/dummy_controller.rb
+++ test/dummy/app/controllers/dummy_controller.rb
@@ -1,4 +1,10 @@
 class DummyController < ApplicationController
   def index
   end
+
+  def assets
+    # Send the compiled application.html as a string so that we can test asset
+    # compilation.
+    render json: Dummy::Application::assets.find_asset("application.html").source
+  end
 end
