88f4c0c0223920a1be27dc08e57cf744487a9c31
Andrew <andrewhuth2@live.com>
2014-03-06 14:07:23 -0500

    Modularize action view helpers

--- lib/emcee.rb
+++ lib/emcee.rb
@@ -5,3 +5,19 @@ require "emcee/helpers/sprockets_view"
 require "emcee/helpers/sprockets_compressing"
 
 require "emcee/railtie"
+
+module ActionView
+  module Helpers
+    module AssetUrlHelper
+      include Emcee::Helpers::ActionView::AssetUrlHelper
+
+      # Modify ActionView to recognize html files and the '/elements' path.
+      ASSET_EXTENSIONS.merge!({ html: '.html' })
+      ASSET_PUBLIC_DIRECTORIES.merge!({ html: '/elements' })
+    end
+
+    module AssetTagHelper
+      include Emcee::Helpers::ActionView::AssetTagHelper
+    end
+  end
+end
--- lib/emcee/helpers/action_view.rb
+++ lib/emcee/helpers/action_view.rb
@@ -1,34 +1,36 @@
-module ActionView
+module Emcee
   module Helpers
-    module AssetUrlHelper
-      # Modify ActionView to recognize html files and the '/elements' path.
-      ASSET_EXTENSIONS.merge!({ html: '.html' })
-      ASSET_PUBLIC_DIRECTORIES.merge!({ html: '/elements' })
-
-      # Convenience method for html based on javascript_path.
-      def html_path(source, options = {})
-        path_to_asset(source, { type: :html }.merge!(options))
+    module ActionView
+      module AssetUrlHelper
+        # Convenience method for html based on javascript_path.
+        def html_path(source, options = {})
+          path_to_asset(source, { type: :html }.merge!(options))
+        end
+        alias_method :path_to_html, :html_path
       end
-      alias_method :path_to_html, :html_path
-    end
 
-    module AssetTagHelper
-      # Custom view helper used to create an html import. Will search the asset
-      # directories for the sources.
-      #
-      #   html_import_tag("navigation")
-      #
-      def html_import_tag(*sources)
-        options = sources.extract_options!.stringify_keys
-        path_options = options.extract!('protocol').symbolize_keys
+      module AssetTagHelper
+        def path_to_html(source, options = {})
+          path_to_asset(source, { type: :html }.merge!(options))
+        end
 
-        sources.uniq.map { |source|
-          tag_options = {
-            "rel" => "import",
-            "href" => path_to_html(source, path_options)
-          }.merge!(options)
-          tag(:link, tag_options)
-        }.join("\n").html_safe
+        # Custom view helper used to create an html import. Will search the asset
+        # directories for the sources.
+        #
+        #   html_import_tag("navigation")
+        #
+        def html_import_tag(*sources)
+          options = sources.extract_options!.stringify_keys
+          path_options = options.extract!('protocol').symbolize_keys
+
+          sources.uniq.map { |source|
+            tag_options = {
+              "rel" => "import",
+              "href" => path_to_html(source, path_options)
+            }.merge!(options)
+            tag(:link, tag_options)
+          }.join("\n").html_safe
+        end
       end
     end
   end
