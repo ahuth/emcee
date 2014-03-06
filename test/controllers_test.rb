31dfcbdaafb08460befc33eeb58e199cc54676b7
Andrew <andrewhuth2@live.com>
2014-03-05 21:31:54 -0500

    Test compiled application.html in dummy app

--- test/controllers_test.rb
+++ test/controllers_test.rb
@@ -13,4 +13,25 @@ class ControllersTest < ActionController::TestCase
     get :index
     assert_select "link[rel='import']"
   end
+
+  # To test the contents of our compiled application.html, we have a custom route
+  # and controller action. The controller action renders the compiled file as a
+  # json response, which we can test against here.
+  test "the test files should get compiled and concatenated" do
+    get :assets
+    assert_response :success
+    assert_equal @response.body, <<-EOS
+<script>var life = "is good";
+</script>
+<p>test4</p>
+<style>
+p {
+  color: pink;
+}
+</style>
+<p>test3</p>
+<p>test2</p>
+<p>test1</p>
+EOS
+  end
 end
