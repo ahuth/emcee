661d92dd29489e50aa47162c94ceb0b3bca4a31c
Andrew <andrewhuth2@live.com>
2014-03-10 22:26:31 -0400

    Revert "Changed dummy#assets to render to json any asset"
    This reverts commit 1444808f50f104c56c80ad044eb07b55cb5430d4.

--- test/dummy/app/controllers/dummy_controller.rb
+++ test/dummy/app/controllers/dummy_controller.rb
@@ -3,16 +3,8 @@ class DummyController < ApplicationController
   end
 
   def assets
-    # Render an asset as a json string so that we can test it.
-    path = params[:file]
-    compiled = Dummy::Application::assets.find_asset(path)
-
-    if compiled.nil?
-      render json: nil
-    else
-      # The source method here causes the asset pipeline to compile and
-      # concatenate the asset.
-      render json: compiled.source
-    end
+    # Send the compiled application.html as a string so that we can test asset
+    # compilation.
+    render json: Dummy::Application::assets.find_asset("application.html").source
   end
 end
