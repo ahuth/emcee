959cb4f8097e29411fff1519672681b8da10d77f
Andrew <andrewhuth2@live.com>
2014-06-11 07:37:53 -0400

    Removed unneeded comments from compressor

--- lib/emcee/html_compressor.rb
+++ lib/emcee/html_compressor.rb
@@ -1,41 +1,13 @@
 module Emcee
-  # HtmlCompressor is our basic implementation of an html minifier. For
-  # sprockets to use it, it must have a compress method that only accepts a
-  # string and returns the compressed output.
-  #
-  # Our implementation only strips out html and javascript comments, and
-  # removes blank lines.
-  #
+  # HTML compressor that strips out comments and blank lines. For Sprockets to
+  # use this, it must have a `compress` method that takes a string and returns
+  # the compressed output.
   class HtmlCompressor
-    # Match html comments.
-    #
-    #   <!--
-    #   Comments
-    #   -->
-    #
-    HTML_COMMENTS = /\<!\s*--(?:.*?)(?:--\s*\>)/m
-
-    # Match multi-line javascript/css comments.
-    #
-    #   /*
-    #   Comments
-    #   */
-    #
+    HTML_COMMENTS     = /\<!\s*--(?:.*?)(?:--\s*\>)/m
     JS_MULTI_COMMENTS = /\/\*(?:.*?)\*\//m
+    JS_COMMENTS       = /\/\/.*$/
+    BLANK_LINES       = /^[\s]*$\n/
 
-    # Match single-line javascript comments.
-    #
-    #   // Comments
-    #
-    # This does not have the /m modifier, because we only want it to match a
-    # single line at a time.
-    #
-    JS_COMMENTS = /\/\/.*$/
-
-    # Match blank lines.
-    BLANK_LINES = /^[\s]*$\n/
-
-    # Remove comments and blank lines from our html.
     def compress(string)
       ops = [HTML_COMMENTS, JS_MULTI_COMMENTS, JS_COMMENTS, BLANK_LINES]
 
