f8a04a5f4cb1f289001cd426a4fa473906dc094f
Andrew <andrewhuth2@live.com>
2014-06-11 07:24:05 -0400

    Remove 1 directory/module level

---
+++ lib/emcee/html_compressor.rb
@@ -0,0 +1,47 @@
+module Emcee
+  # HtmlCompressor is our basic implementation of an html minifier. For
+  # sprockets to use it, it must have a compress method that only accepts a
+  # string and returns the compressed output.
+  #
+  # Our implementation only strips out html and javascript comments, and
+  # removes blank lines.
+  #
+  class HtmlCompressor
+    # Match html comments.
+    #
+    #   <!--
+    #   Comments
+    #   -->
+    #
+    HTML_COMMENTS = /\<!\s*--(?:.*?)(?:--\s*\>)/m
+
+    # Match multi-line javascript/css comments.
+    #
+    #   /*
+    #   Comments
+    #   */
+    #
+    JS_MULTI_COMMENTS = /\/\*(?:.*?)\*\//m
+
+    # Match single-line javascript comments.
+    #
+    #   // Comments
+    #
+    # This does not have the /m modifier, because we only want it to match a
+    # single line at a time.
+    #
+    JS_COMMENTS = /\/\/.*$/
+
+    # Match blank lines.
+    BLANK_LINES = /^[\s]*$\n/
+
+    # Remove comments and blank lines from our html.
+    def compress(string)
+      ops = [HTML_COMMENTS, JS_MULTI_COMMENTS, JS_COMMENTS, BLANK_LINES]
+
+      ops.reduce(string) do |output, op|
+        output.gsub(op, "")
+      end
+    end
+  end
+end
--- lib/emcee/railtie.rb
+++ lib/emcee/railtie.rb
@@ -1,15 +1,15 @@
-require "emcee/processors/html_processor"
-require "emcee/compressors/html_compressor"
+require "emcee/html_processor"
+require "emcee/html_compressor"
 
 module Emcee
   class Railtie < Rails::Railtie
     initializer :add_html_processor do |app|
       app.assets.register_mime_type "text/html", ".html"
-      app.assets.register_preprocessor "text/html", Processors::HtmlProcessor
+      app.assets.register_preprocessor "text/html", HtmlProcessor
     end
 
     initializer :add_html_compressor do |app|
-      app.assets.html_compressor = Compressors::HtmlCompressor.new
+      app.assets.html_compressor = HtmlCompressor.new
     end
   end
 end
