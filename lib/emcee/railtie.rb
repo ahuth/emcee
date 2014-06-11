3c76ca4287533ab0fddfa374c56bcca6e2398f10
Andrew <andrewhuth2@live.com>
2014-06-11 07:48:00 -0400

    Remove unneeded comments from the gem's railtie

--- lib/emcee/railtie.rb
+++ lib/emcee/railtie.rb
@@ -15,25 +15,21 @@ module Emcee
 end
 
 # Monkey patch Sprockets-rails so that loose html files are handled correctly.
-# In the /assets/elements directory, loose html files should be compiled into
-# application.html. They should not be carried over as single files.
 #
 # Not sure that modifying another gem's railtie is a good idea, but it works
 # for now.
 module Sprockets
   class Railtie
-    # Horrible hack: remove this 'constant' so we can redefine it without ruby
+    # Remove the LOOSE_APP_ASSETS constant so we can modify it without Ruby
     # complaining.
     remove_const(:LOOSE_APP_ASSETS) if defined?(LOOSE_APP_ASSETS)
 
-    # Ugly hack #1: redefine this 'constant' to recognize html files. Is there
-    # a way to extend instead of redefining?
+    # Add .html to the loose app assets file extensions.
     LOOSE_APP_ASSETS = lambda do |filename, path|
       path =~ /app\/assets/ && !%w(.js .css .html).include?(File.extname(filename))
     end
 
-    # Ugly hack #2: same as above (#1). Is there a way to extend this instead
-    # of redefining?
+    # Add the .html extension to the 'precompile' regex.
     config.assets.precompile = [LOOSE_APP_ASSETS, /(?:\/|\\|\A)application\.(css|js|html)$/]
   end
 end
