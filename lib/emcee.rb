e15a55a201680f4b025e405b6bce8a26816af7a3
Andrew <andrewhuth2@live.com>
2014-06-15 23:55:22 -0400

    Rename sprockets view helpers

--- lib/emcee.rb
+++ lib/emcee.rb
@@ -2,7 +2,7 @@ require "emcee/version"
 
 require "emcee/helpers/asset_url_helper"
 require "emcee/helpers/asset_tag_helper"
-require "emcee/helpers/view_helpers"
+require "emcee/helpers/sprockets_helper"
 
 require "emcee/loose_assets"
 require "emcee/railtie"
