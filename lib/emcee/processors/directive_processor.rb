bcfe5e207e0cd0747a068b3b4373a591ad191626
Andrew <andrewhuth2@live.com>
2014-06-15 23:42:48 -0400

    Comments

--- lib/emcee/processors/directive_processor.rb
+++ lib/emcee/processors/directive_processor.rb
@@ -1,48 +1,18 @@
 module Emcee
-  # DirectiveProcessor processes html files by doing 4 things:
-  #
-  # 1. Stripping out asset pipeline directives and processing the associated
-  #    files.
-  # 2. Stripping out html imports and processing those files.
-  # 3. Stripping out external stylesheet includes, and inlining those sheets
-  #    where they were called.
-  # 4. Stripping out external script tags, and inlining those scripts where
-  #    they were called.
-  #
-  # It inherits from Sprocket::DirectiveProcessor, which does most of the
-  # work for us.
-  #
+  # The `DirectiveProcessor` is responsible for parsing and evaluating
+  # directive comments in a source file.
   class DirectiveProcessor < Sprockets::DirectiveProcessor
     # Matches the entire header/directive block. This is everything from the
     # top of the file, enclosed in html comments.
-    #
-    # ---
-    #
-    # \A                matches the beginning of the string
-    # (?m:\s*)          matches whitespace, including \n
-    # (<!--(?m:.*?)-->) matches html comments and their content
-    #
     HEADER_PATTERN = /\A((?m:\s*)(<!--(?m:.*?)-->))+/
 
     # Matches the an asset pipeline directive.
     #
     #   *= require_tree .
     #
-    # ---
-    #
-    # ^         matches the beginning of the line
-    # \W*       matches any non-word character, zero or more times
-    # =         matches =, obviously
-    # \s*       matches any whitespace character, zero or more times
-    # (\w+.*?)  matches a group of characters, starting with a letter or number
-    # $         matches the end of the line
-    #
     DIRECTIVE_PATTERN = /^\W*=\s*(\w+.*?)$/
 
-    # Render takes the actual text of the file and does our processing to it.
-    # This is based on the standard render method on Sprockets's
-    # DirectiveProcessor.
-    #
+    # Implement `render` so that it uses our own regex patterns.
     def render(context, locals)
       @context = context
       @pathname = context.pathname
