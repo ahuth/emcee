6f1054dc6bae27a8f95e0285539256a45857a7bb
Andrew <andrewhuth2@live.com>
2014-06-16 07:00:09 -0400

    Remove unnecessary mime type declaration

--- lib/emcee/railtie.rb
+++ lib/emcee/railtie.rb
@@ -7,7 +7,6 @@ require "emcee/compressors/html_compressor"
 module Emcee
   class Railtie < Rails::Railtie
     initializer :add_html_processor do |app|
-      app.assets.register_mime_type "text/html", ".html"
       app.assets.register_preprocessor "text/html", DirectiveProcessor
     end
 
