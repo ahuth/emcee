23b14b6a1df59fb7a043e0f99b12bd5b1cc6b414
Andrew <andrewhuth2@live.com>
2014-06-15 23:48:31 -0400

    Split sprockets-rails monkey patch

--- lib/emcee.rb
+++ lib/emcee.rb
@@ -4,4 +4,5 @@ require "emcee/helpers/action_view/asset_url_helper"
 require "emcee/helpers/action_view/asset_tag_helper"
 require "emcee/helpers/sprockets/view_helpers"
 
+require "emcee/loose_assets"
 require "emcee/railtie"
