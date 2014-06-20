b26a3158534b1d9c640a172f60c25bde7b676553
Andrew <andrewhuth2@live.com>
2014-06-20 07:08:48 -0400

    Rename 'elements' directory to 'components'

--- lib/generators/emcee/install/install_generator.rb
+++ lib/generators/emcee/install/install_generator.rb
@@ -3,16 +3,16 @@ module Emcee
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
-      desc "Adds an html import tag into your layout, adds a manifest to assets/elements/application.html, and adds a vendor/assets/elements directory"
+      desc "Adds an html import tag into your layout, adds a manifest to assets/components/application.html, and adds a vendor/assets/components directory"
 
       def copy_application_manifest
-        empty_directory "app/assets/elements"
-        copy_file "application.html", "app/assets/elements/application.html"
+        empty_directory "app/assets/components"
+        copy_file "application.html", "app/assets/components/application.html"
       end
 
       def create_vendor_directory
-        empty_directory "vendor/assets/elements"
-        create_file "vendor/assets/elements/.keep"
+        empty_directory "vendor/assets/components"
+        create_file "vendor/assets/components/.keep"
       end
 
       def add_html_import_to_layout
--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class Helpers < ActionView::TestCase
   test "html_import should work" do
-    assert_equal "<link href=\"/elements/test.html\" rel=\"import\" />", html_import_tag("test")
+    assert_equal "<link href=\"/components/test.html\" rel=\"import\" />", html_import_tag("test")
   end
 end
