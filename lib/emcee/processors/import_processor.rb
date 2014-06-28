5882dbb68e1bbdc5f0af7a89afffb2b458eeb8f0
Andrew <andrewhuth2@live.com>
2014-06-28 01:09:19 -0400

    Parse HTMl for imports

--- lib/emcee/processors/import_processor.rb
+++ lib/emcee/processors/import_processor.rb
@@ -1,44 +1,34 @@
+require 'nokogiri'
+
 module Emcee
   # ImportProcessor scans a file for html imports and adds them to the current
   # required assets.
   class ImportProcessor
-    # Match an html import tag.
-    #
-    #   <link rel="import" href="assets/example.html">
-    #
-    IMPORT_PATTERN = /^\s*<link .*rel=["']import["'].*>$/
-
-    # Match the path from the href attribute of an html import or stylesheet
-    # include tag. Captures the actual path.
-    #
-    #   href="/assets/example.css"
-    #
-    HREF_PATH_PATTERN = /href=["'](?<path>[\w\.\/-]+)["']/
-
     def initialize(context)
       @context = context
       @directory = File.dirname(context.pathname)
     end
 
     def process(data)
-      require_assets(data)
-      remove_imports(data)
+      doc = Nokogiri::HTML("<body>#{data}</body>")
+      require_assets(doc)
+      remove_imports(doc)
+      doc.at("body").children.to_s.lstrip
     end
 
     private
 
-    def require_assets(data)
-      data.scan(IMPORT_PATTERN) do |import_tag|
-        path = import_tag[HREF_PATH_PATTERN, :path]
-        return unless path
-
-        absolute_path = File.absolute_path(path, @directory)
-        @context.require_asset(absolute_path)
+    def require_assets(doc)
+      doc.css("link[rel='import']").each do |node|
+        path = File.absolute_path(node.attribute("href"), @directory)
+        @context.require_asset(path)
       end
     end
 
-    def remove_imports(data)
-      data.gsub(IMPORT_PATTERN, "")
+    def remove_imports(doc)
+      doc.search("link[rel='import']").each do |node|
+        node.remove
+      end
     end
   end
 end
