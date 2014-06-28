b13ddb81ad222a91399a33ae8337afe16c37205e
Andrew <andrewhuth2@live.com>
2014-06-27 23:35:14 -0400

    Test CoffeeScript processing

--- test/processors_test.rb
+++ test/processors_test.rb
@@ -3,6 +3,7 @@ require 'emcee/processors/import_processor'
 require 'emcee/processors/script_processor'
 require 'emcee/processors/stylesheet_processor'
 
+require 'coffee-rails'
 require 'sass'
 
 # Create a stub of Sprocket's Context class, so we can test if we're 'requiring'
@@ -84,6 +85,20 @@ class ProcessorsTest < ActiveSupport::TestCase
       <style>/* contents */
       </style>
       <script src="test.js"></script>
+      <p>test</p>
+    }
+  end
+
+  test "processing CoffeeScript should work" do
+    @body.gsub!("test.js", "test.js.coffee")
+    processor = Emcee::ScriptProcessor.new
+    processed = processor.process(@context, @body, @directory)
+
+    assert_equal processed, %q{
+      <link rel="import" href="test.html">
+      <link rel="stylesheet" href="test.css">
+      <script>/* contents */
+      </script>
       <p>test</p>
     }
   end
