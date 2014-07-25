2e79ef1a18108ba5f1939e0be36caec919a385fa
Jonathan Soeder <jonathan.soeder@gmail.com>
2014-07-24 19:19:59 -0500

    improving generator code, fixing whitespace and indentation for the slim and haml layouts. adds option to specify alternate layout path

--- lib/generators/emcee/install/install_generator.rb
+++ lib/generators/emcee/install/install_generator.rb
@@ -5,6 +5,8 @@ module Emcee
 
       desc "Adds an html import tag into your layout, adds a manifest to assets/components/application.html, and adds a vendor/assets/components directory"
 
+      class_option :layout, :type => :string, :desc => "The path of the layout file to inject the html import tag into"
+
       def copy_application_manifest
         empty_directory "app/assets/components"
         copy_file "application.html", "app/assets/components/application.html"
@@ -18,11 +20,9 @@ module Emcee
       def add_html_import_to_layout
         case
           when erb?
-            insert_html_import("<%= html_import_tag \"application\", \"data-turbolinks-track\" => true %>\n  ", before: "<%= csrf_meta_tags %>")
-          when haml?
-            insert_html_import("= html_import_tag \"application\", \"data-turbolinks-track\" => true\n  ", before: "= csrf_meta_tags")
-          when slim?
-            insert_html_import("= html_import_tag \"application\", \"data-turbolinks-track\" => true\n  ", before: "= csrf_meta_tags")
+            insert_html_import("<%= html_import_tag \"application\", \"data-turbolinks-track\" => true %>", before: "<%= csrf_meta_tags %>")
+          when haml? || slim?
+            insert_html_import("= html_import_tag \"application\", \"data-turbolinks-track\" => true", before: "= csrf_meta_tags")
         end
       end
 
@@ -33,26 +33,30 @@ module Emcee
       private
 
       def insert_html_import(content, options={})
-        insert_into_file(layout_file, content, options)
+        # if this is a slim or haml file we need to match the indentation level
+        if options[:before] && (line = layout_file.read.lines.detect {|l| l.include?(options[:before].to_s) })
+          indentation = line.match(/^\s+/).to_s
+          content = content + "\n#{indentation}"
+        end
+
+        insert_into_file(layout_file.to_s, content, options)
       end
 
       def erb?
-        layout_file.match(/\.erb/)
+        layout_file.extname.match(/\.erb/)
       end
 
       def haml?
-        layout_file.match(/\.haml/)
+        layout_file.extname.match(/\.haml/)
       end
 
       def slim?
-        layout_file.match(/\.slim/)
+        layout_file.extname.match(/\.slim/)
       end
 
       def layout_file
-        @layout_file ||= begin
-                           file = Pathname(Dir[Rails.root.join("app","views","layouts","application*")].first)
-                           file.relative_path_from(Rails.root).to_s
-                         end
+        file = Pathname(options["layout"] || Dir[Rails.root.join("app","views","layouts","application*")].first)
+        file.relative? ? file : file.relative_path_from(Rails.root)
       end
 
     end
