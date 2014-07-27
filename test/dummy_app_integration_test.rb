cff4599bcf2e10eed294e9f48ccee1b678284e30
Andrew <andrewhuth2@live.com>
2014-07-27 18:05:26 -0400

    Delete test files

--- test/dummy_app_integration_test.rb
+++ test/dummy_app_integration_test.rb
@@ -11,33 +11,6 @@ class DummyAppIntegrationTest < ActionController::TestCase
   test "the test files should get compiled and concatenated" do
     get :assets
     assert_equal @response.body, <<-EOS.strip_heredoc
-      <script>var life = "is good";
-      </script>
-      <p>test4</p>
-      <style>p {
-        color: red; }
-      </style>
-      <script>(function() {
-        var hello;
-        hello = "world";
-      }).call(this);
-      </script>
-      <p>compiled scss and CoffeeScript</p>
-      <style>p {
-        color: pink;
-      }
-      </style>
-      <p>test3</p>
-      <p>test2</p>
-      <p>test1</p>
-      <polymer-element name="test6" attributes="source">
-        <template>
-          <p hidden?="{{ hidden }}">hidden</p>
-          <img src="{{ source }}">
-        </template>
-      </polymer-element>
-      <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-      <p>External script</p>
     EOS
   end
 end
