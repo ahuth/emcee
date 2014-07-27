6a8c852ae5942817101d22269a448be60acfb5bb
Andrew <andrewhuth2@live.com>
2014-07-27 18:20:43 -0400

    Test an import with simple css

--- test/dummy_app_integration_test.rb
+++ test/dummy_app_integration_test.rb
@@ -12,6 +12,11 @@ class DummyAppIntegrationTest < ActionController::TestCase
     get :assets
     assert_equal @response.body, <<-EOS.strip_heredoc
       <p>Test simple import</p>
+      <style>p {
+        color: red;
+      }
+      </style>
+      <p>Test css</p>
     EOS
   end
 end
