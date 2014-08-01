3da3a996017541135f21647a98c838df2684b745
Andrew <andrewhuth2@live.com>
2014-08-01 14:36:46 -0400

    Add Node class

---
+++ lib/emcee/node.rb
@@ -0,0 +1,25 @@
+module Emcee
+  # Document is responsible for interacting with individual html nodes that
+  # make up the parsed document.
+  class Node
+    def initialize(parser_node)
+      @parser_node = parser_node
+    end
+
+    def href
+      @parser_node.attribute("href")
+    end
+
+    def src
+      @parser_node.attribute("src")
+    end
+
+    def remove
+      @parser_node.remove
+    end
+
+    def replace(new_node)
+      @parser_node.replace(new_node)
+    end
+  end
+end
---
+++ test/node_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+require 'emcee/node.rb'
+
+# Create a stub of the native nodes used by the html parser, so we can test if
+# we're sending the correct messages to it.
+class ParserNodeStub
+  attr_reader :removed, :replaced
+
+  def initialize
+    @attributes = { href: "test.css", src: "test.js" }
+  end
+
+  def attribute(name)
+    @attributes[name.to_sym]
+  end
+
+  def remove
+    @removed = true
+  end
+
+  def replace(new_node)
+    @replaced = true
+  end
+end
+
+class NodeTest < ActiveSupport::TestCase
+  setup do
+    @parser_node = ParserNodeStub.new
+    @node = Emcee::Node.new(@parser_node)
+  end
+
+  test "should have href attribute" do
+    assert_equal @node.href, "test.css"
+  end
+
+  test "should have src attribute" do
+    assert_equal @node.src, "test.js"
+  end
+
+  test "should have remove method" do
+    @node.remove
+    assert @parser_node.removed
+  end
+
+  test "should have replace method" do
+    new_parser_node = ParserNodeStub.new
+    new_node = Emcee::Node.new(new_parser_node)
+    @node.replace(new_node)
+    assert @parser_node.replaced
+  end
+end
