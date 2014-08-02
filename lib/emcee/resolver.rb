347d82cab12461d2e5ed49401b3071841227c121
Andrew <andrewhuth2@live.com>
2014-08-02 08:48:08 -0400

    Comments

--- lib/emcee/resolver.rb
+++ lib/emcee/resolver.rb
@@ -6,14 +6,20 @@ module Emcee
       @directory = File.dirname(context.pathname)
     end
 
+    # Declare a file as a dependency to Sprockets. The dependency will be
+    # included in the application's html bundle.
     def require_asset(path)
       @context.require_asset(path)
     end
 
+    # Return the contents of a file. Does any required processing, such as SCSS
+    # or CoffeeScript.
     def evaluate(path)
       @context.evaluate(path)
     end
 
+    # Indicate if an asset should be inlined or not. References to files at an
+    # external web address, for example, should not be inlined.
     def should_inline?(path)
       path !~ /\A\/\//
     end
